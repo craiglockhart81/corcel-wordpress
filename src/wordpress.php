0ca4fea4527b947409c9bcb05aed2ba1129a9677
Developer <17724697+craiglockhart81@users.noreply.github.com>
2023-10-23 12:12:22 +0100

    Initial Commit

---
+++ src/wordpress.php
@@ -0,0 +1,15 @@
+<?php
+
+if (! function_exists('apply_filters')) {
+    /**
+     * Add an element to an array using "dot" notation if it doesn't exist.
+     *
+     * @param  array  $array
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return array
+     */
+    function apply_filters($array, $key, $value)
+    {
+    }
+}
